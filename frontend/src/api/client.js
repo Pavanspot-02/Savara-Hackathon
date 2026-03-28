4267e19fe64b52769ea056ccadca4fa00cb2809c
Pavankumar D <pavanspot02.com>
2026-03-28 19:46:45 +0530

    Add deployment config

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: window.location.hostname === 'localhost' 
+    ? '/api' 
+    : 'https://learnsync-api.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
